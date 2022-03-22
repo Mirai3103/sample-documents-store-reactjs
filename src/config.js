4db87e85822e102f8b699f4126fd5a3047996bd6
Laffy <huuhoag1412@gmail.com>
2022-03-22 13:21:54 +0700

    hoan thien san pham

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const SECRET_KEY = '13092003';
-const BASE_URL = 'https://sampledocumentapi.herokuapp.com/';
+const BASE_URL = 'https://sampledocumentapi.herokuapp.com';
 export { SECRET_KEY, BASE_URL };
 export default function generateToken(key) {
   return btoa(`${key}${SECRET_KEY}`);
